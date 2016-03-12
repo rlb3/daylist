d3d8e91d91e15f8b01be9557313bf022bdd7c907
Robert Boone <robert@rlb3.com>
2016-03-12 12:08:57 -0600

    use maps

--- lib/daylist.ex
+++ lib/daylist.ex
@@ -1,31 +1,35 @@
 defmodule Daylist do
-  @days [:sun, :mon, :tues, :wed, :thurs, :fri, :sat]
+  @by_atom %{sun: 0, mon: 1, tues: 2, wed: 3, thurs: 4, fri: 5, sat: 6}
+  @by_index %{0 => :sun, 1 => :mon, 2 => :tues, 3 => :wed, 4 => :thurs, 5 => :fri, 6 => :sat}
 
   def run({first,second}) do
     cond do
-      consecutive?(first, second) -> Enum.slice(@days, first_index(first)..second_index(second))
-      overlaping?(first, second) -> Enum.slice(@days, first_index(first)..last_index) ++ Enum.slice(@days, 0..second_index(second))
+      consecutive?(first, second) -> build_range(first, second) |> day_range
+      overlaping?(first, second) -> (build_range(first, :sat) |> day_range) ++ (build_range(:sun, second) |> day_range)
     end
   end
 
-  defp consecutive?(first, second) do
+  defp consecutive?(first, second) when is_atom(first) and is_atom(second) do
     first_index(first) < second_index(second)
   end
 
-  defp overlaping?(first, second) do
+  defp overlaping?(first, second) when is_atom(first) and is_atom(second) do
     first_index(first) > second_index(second)
   end
 
-  defp first_index(value) do
-    Enum.find_index(@days, &(&1 == value))
+  defp first_index(value) when is_atom(value) do
+    @by_atom[value]
   end
 
-  defp second_index(value) do
-    Enum.find_index(@days, &(&1 == value))
+  defp second_index(value) when is_atom(value)do
+    @by_atom[value]
   end
 
-  defp last_index do
-    last = List.last(@days)
-    Enum.find_index(@days, &(&1 == last))
+  defp build_range(first, second) do
+    first_index(first)..second_index(second)
+  end
+
+  defp day_range(range) do
+    range |> Enum.map(&(@by_index[&1]))
   end
 end
